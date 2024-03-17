b74631ccb9c4c3e9d3dc88052a706eaba272523e
Elcio Nakashima <elciok@gmail.com>
2024-03-17 16:29:54 -0300

    After mix new

---
+++ lib/sonyflakex.ex
@@ -0,0 +1,18 @@
+defmodule Sonyflakex do
+  @moduledoc """
+  Documentation for `Sonyflakex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sonyflakex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sonyflakex_test.exs
@@ -0,0 +1,8 @@
+defmodule SonyflakexTest do
+  use ExUnit.Case
+  doctest Sonyflakex
+
+  test "greets the world" do
+    assert Sonyflakex.hello() == :world
+  end
+end
