e92955621ecc84e50a493b8d7398e4bd3f5436be
Elcio Nakashima <elciok@gmail.com>
2024-03-24 11:59:24 -0300

    GenServer to keep state and generate IDs

--- lib/sonyflakex.ex
+++ lib/sonyflakex.ex
@@ -3,16 +3,58 @@ defmodule Sonyflakex do
   Documentation for `Sonyflakex`.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  alias Sonyflakex.{State, Generator, Time}
 
-      iex> Sonyflakex.hello()
-      :world
+  # @doc """
+  # Hello world.
 
-  """
-  def hello do
-    :world
+  # ## Examples
+
+  #     iex> Sonyflakex.hello()
+  #     :world
+
+  # """
+  # def hello do
+  #   :world
+  # end
+
+  def start_link(args) do
+    GenServer.start_link(
+      __MODULE__,
+      args,
+      name: __MODULE__
+    )
+  end
+
+  @impl GenServer
+  def init(_args) do
+    {:ok, State.new()}
+  end
+
+  @impl GenServer
+  def handle_call(:next_id, _from, state) do
+    generate_next_id(state)
+  end
+
+  defp generate_next_id({start_time, elapsed_time, machine_id, _sequence} = state) do
+    case Generator.next_id(state) do
+      {:ok, new_id, new_state} ->
+        {:reply, new_id, new_state}
+      {:error, :overflow} ->
+        # wait until next timestamp that will reset sequence
+        current_time = Time.current_elapsed_time(start_time)
+        wait_ms = Time.time_until_next_timestamp(elapsed_time, current_time)
+        if wait_ms > 0, do: Process.sleep(wait_ms)
+
+        # try again after waking up
+        State.create_state(start_time, elapsed_time + 1, machine_id, 0)
+        |> generate_next_id()
+    end
+  end
+
+  def next_id() do
+    GenServer.call(__MODULE__, :next_id)
   end
 end
--- test/sonyflakex_test.exs
+++ test/sonyflakex_test.exs
@@ -2,7 +2,33 @@ defmodule SonyflakexTest do
   use ExUnit.Case
   doctest Sonyflakex
 
-  test "greets the world" do
-    assert Sonyflakex.hello() == :world
+  describe "next_id/0" do
+    test "generates a new id" do
+      {:ok, _pid} = Sonyflakex.start_link([])
+      id = Sonyflakex.next_id()
+      assert is_integer(id)
+    end
+
+    test "generates unique ids in strict ascending order" do
+      num_ids = 5_000
+      {:ok, _pid} = Sonyflakex.start_link([])
+
+      ids = Enum.map(1..num_ids, fn _item -> Sonyflakex.next_id() end)
+
+      assert num_ids == length(Enum.uniq(ids))
+
+      assert is_ascending(ids)
+    end
+  end
+
+  # helper function to check list order
+  def is_ascending([]), do: true
+  def is_ascending([_]), do: true
+  def is_ascending([head1, head2 | tail]) do
+    if head1 >= head2 do
+      false
+    else
+      is_ascending([head2 | tail])
+    end
   end
 end
